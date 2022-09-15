16a1cda7f1743d430ab7a491e84fa1a068685c93
eswardatt <eswardatta277@gmail.com>
2022-09-15 18:09:43 +0530

    read single/multiple files

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using System.Data;
 
 Console.WriteLine("Hello, World!");
 excellrdr excellreader = new excellrdr();
+DataSet ds = excellreader.GetAllfiles();
+DataTable dt = ds.Tables[0];
+//int i = dt.Rows.Count;
+//excellreader.GetAllfiles();
+//Console.WriteLine(i);
 
-DataTable dt = excellreader.Get();
-int i = dt.Rows.Count;
-Console.WriteLine(i);
 foreach (DataRow row in dt.Rows)
 {
   Console.WriteLine("Id : {0} && Name : {1}",  row["id"].ToString(), row["name"].ToString());
--- excellrdr.cs
+++ excellrdr.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace excellreader
 {
     internal class excellrdr
     {
-        public DataTable Get()
+        public DataTable GetSinglefile()
         {
             string conStr = "";
             string FilePath = "D:/Core";
@@ -65,6 +64,74 @@ namespace excellreader
             return dt;
 
 
+        }
+
+        DataTable GetGroupdata(string filename)
+        {
+            string conStr = "";
+            string FilePath = "D:/Core/"+ filename;
+            string Extension = ".xlsx";
+            switch (Extension)
+            {
+                case ".xls":
+                    conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Mode=ReadWrite;Extended Properties=Excel 12.0 Xml;HDR={1};IMEX=1";//ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                    break;
+                case ".xlsx":
+                    // conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Mode=ReadWrite;Extended Properties=Excel 12.0 Xml;HDR={1};IMEX=1";//ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                    conStr = $"Provider = Microsoft.ACE.OLEDB.12.0; Data Source ={FilePath}; Excel 12.0 Xml; HDR = YES";
+                    break;
+
+            }
+            conStr = String.Format(conStr, FilePath, "Yes");
+            OleDbConnection connExcel = new OleDbConnection(conStr);
+            OleDbCommand cmdExcel = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+
+            DataTable dt = new DataTable();
+
+            cmdExcel.Connection = connExcel;
+            //Get the name of First Sheet
+
+            connExcel.Open();
+
+            DataTable dtExcelSchema;
+
+            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            string SheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+
+
+
+            connExcel.Close();
+            //Read Data from First Sheet
+
+            connExcel.Open();
+
+            cmdExcel.CommandText = "SELECT * From [" + SheetName + "]";
+
+
+            //oda.TableMappings.Add("Table", "TestTable");
+
+            oda.SelectCommand = cmdExcel;
+
+            oda.Fill(dt);
+
+            connExcel.Close();
+            return dt;
+
+
+        }
+        public DataSet GetAllfiles()
+        {
+            DirectoryInfo d = new DirectoryInfo(@"D:\Core"); //Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.xlsx"); //Getting Text files
+            
+            DataSet ds = new DataSet();
+            foreach (FileInfo file in Files)
+            {
+                ds.Tables.Add(GetGroupdata(file.Name));
+            }
+           return ds;
         }
     }
  
