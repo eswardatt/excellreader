ea6fc65083a38846bd80735a906df3b104ff994b
eswardatt <eswardatta277@gmail.com>
2022-09-15 17:38:46 +0530

    added files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using excellreader;
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+excellrdr excellreader = new excellrdr();
+
+DataTable dt = excellreader.Get();
+int i = excellreader.Get().Rows.Count;
+Console.WriteLine(i);
+foreach (DataRow row in dt.Rows)
+{
+  Console.WriteLine("Id : {0} && Name : {1}",  row["id"].ToString(), row["name"].ToString());
+}
---
+++ excellrdr.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excellreader
+{
+    internal class excellrdr
+    {
+        public DataTable Get()
+        {
+            string conStr = "";
+            string FilePath = "D:/Core";
+            string Extension = ".xlsx";
+            switch (Extension)
+            {
+                case ".xls":
+                    conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Mode=ReadWrite;Extended Properties=Excel 12.0 Xml;HDR={1};IMEX=1" ;//ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                    break;
+                case ".xlsx":
+                    // conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Mode=ReadWrite;Extended Properties=Excel 12.0 Xml;HDR={1};IMEX=1";//ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                    conStr = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =D:/Core/book.xlsx; Excel 12.0 Xml; HDR = YES";
+                    break;
+
+            }
+            conStr = String.Format(conStr, FilePath, "Yes");
+            OleDbConnection connExcel = new OleDbConnection(conStr);
+            OleDbCommand cmdExcel = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+
+            DataTable dt = new DataTable();
+
+            cmdExcel.Connection = connExcel;
+            //Get the name of First Sheet
+
+            connExcel.Open();
+
+            DataTable dtExcelSchema;
+
+            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            string SheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+
+
+
+            connExcel.Close();
+            //Read Data from First Sheet
+
+            connExcel.Open();
+
+            cmdExcel.CommandText = "SELECT * From [" + SheetName + "]";
+
+
+            //oda.TableMappings.Add("Table", "TestTable");
+
+            oda.SelectCommand = cmdExcel;
+
+            oda.Fill(dt);
+
+            connExcel.Close();
+            return dt;
+
+
+        }
+    }
+ 
+}
